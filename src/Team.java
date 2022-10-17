11890297e29828dfe6416327dadd39304f3b3a02
Felipe Luciano Yaschischin Fraccaroli <115169505+FeliYas@users.noreply.github.com>
2022-10-17 13:11:51 -0300

    chore: add team class

---
+++ src/Team.java
@@ -0,0 +1,17 @@
+public class Team {
+    private String name;
+    private ArrayList<Player> players;
+
+    public void setName(String name){
+        this.name = name;
+        return;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addPlayer(Player player){
+        //this.players.add(player);
+    }
+}
